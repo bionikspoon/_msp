1f5c9d194c60dd6531b79d01a3f3884633125f34
Manu Phatak <bionikspoon@gmail.com>
2016-04-06 17:03:59 -0500

    cleanup js, fix elements

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,10 @@
+/* global siteData */
 require('./navigation');
 require('./skip-link-focus-fix');
 const Masonry = require('masonry-layout');
 const imagesLoaded = require('imagesloaded');
 const malarkey = require('malarkey');
+
 
 (() => {
   // Bind to DOM
@@ -24,6 +26,8 @@ const malarkey = require('malarkey');
   };
   const HIGHLIGHT_CLASS = 'title-highlight';
   const HIGHLIGHT_CLASS_LEGEND = 'title-highlight-legend';
+  const SITE_TITLE = siteData.title;
+  const SITE_DESCRIPTION = siteData.description;
 
   // Events
   function loadingComplete() {
@@ -34,62 +38,145 @@ const malarkey = require('malarkey');
   // Run masonry
   function activateMasonry() {
     const container = document.querySelector('._msp-portfolio__container');
-    if (container) {
-      const masonry = new Masonry(container, options.masonry);
-      imagesLoaded(container, masonry.layout);
+
+    // Guard, no container
+    if (!container) {return;}
+
+    // more setup
+    const header = document.querySelector('._msp-header');
+    const footer = document.querySelector('._msp-footer');
+
+    //noinspection XHTMLIncompatabilitiesJS
+    const body = document.body;
+
+    // Guard, no body, prevent xml xmhtml bugs
+    if (!body) {return;}
+
+    // bind to DOM
+    const masonry = new Masonry(container, options.masonry);
+    imagesLoaded(container, masonry.layout);
+    masonry.on('layoutComplete', handleResize);
+    window.addEventListener('resize', handleResize);
+
+    // init
+    setContainerMinHeight(container, getContainerMinHeight());
+
+    /**
+     * Set container min height when height changes.
+     * @param {number} clientHeight
+     */
+    function handleResize(clientHeight = body.clientHeight) {
+      // Guard, height did not change
+      if (clientHeight === handleResize.height) {return;}
+
+      // update static height
+      handleResize.height = clientHeight;
+
+      // update container
+      setContainerMinHeight(container, getContainerMinHeight());
+    }
+
+    // initialize static height
+    handleResize.height = body.clientHeight;
+
+    /**
+     * Set container min height.
+     *
+     * @param {Element} containerElement
+     * @param {number} minHeight number
+     */
+    function setContainerMinHeight(containerElement, minHeight) {
+      containerElement.style.minHeight = `${minHeight}px`; // eslint-disable-line no-param-reassign
+    }
+
+    /**
+     * Get container minHeight.
+     *
+     * @param {Element} headerElement
+     * @param {Element} footerElement
+     * @param {Element} bodyElement
+     * @returns {number}
+     */
+    function getContainerMinHeight(headerElement = header, footerElement = footer, bodyElement = body) {
+      const windowHeight = bodyElement.clientHeight;
+      const headerHeight = headerElement.offsetHeight;
+      const footerHeight = footerElement.offsetHeight;
+
+      return windowHeight - headerHeight - footerHeight;
     }
   }
 
   // Run malarkey
   function activateMalarkey() {
+    // setup
     const pageTitleContainer = document.querySelector('._msp-header-title');
-    if (!pageTitleContainer) return;
-    const pageTitle = pageTitleContainer.querySelector('#_msp-page-title');
 
+    // Guard
+    if (!pageTitleContainer) {return;}
+
+    // more setup
+    const pageTitle = pageTitleContainer.querySelector('#_msp-page-title');
     const malarkeyHandle = getMalarkeyHandle(pageTitle, options.malarkey);
 
+    // run malarkey
     malarkeyHandle
       .reset()
-      .typePhrase('Manu Phatak')
-      .typePhrase('Full Stack Developer', 2000)
+      .typePhrase(SITE_TITLE)
+      .typePhrase(SITE_DESCRIPTION, 2000)
       .typeLegendTaunt();
 
-    function addHighlight() {
-      pageTitleContainer.classList.add(HIGHLIGHT_CLASS);
-      this();
-    }
-
-    function addHighlightLegend() {
-      pageTitleContainer.classList.add(HIGHLIGHT_CLASS);
-      pageTitleContainer.classList.add(HIGHLIGHT_CLASS_LEGEND);
-      this();
-    }
-
-    function removeHighlight() {
-      pageTitleContainer.classList.remove(HIGHLIGHT_CLASS);
-      this();
-    }
-
-    function removeHighlightLegend() {
-      pageTitleContainer.classList.remove(HIGHLIGHT_CLASS_LEGEND);
-      this();
-    }
-
+    /**
+     * Create malarkey handle with a few custom tasks.
+     */
     function getMalarkeyHandle(el, opts) {
+      // create handle
       const handle = malarkey(el, opts);
+
+      handle.addClass = function addClass(name) {
+        this.call(wrapper);
+        return this;
+
+        function wrapper() {
+          pageTitleContainer.classList.add(name);
+          this();
+        }
+      };
+
+      handle.removeClass = function removeClass(name) {
+        this.call(wrapper);
+        return this;
+
+        function wrapper() {
+          pageTitleContainer.classList.remove(name);
+          this();
+        }
+      };
+
+      /**
+       * Remove highlight and delete text;
+       */
       handle.reset = function reset() {
-        this.call(removeHighlight).delete();
+        this.removeClass(HIGHLIGHT_CLASS).delete();
         return this;
       };
 
+      /**
+       *  Type phrase -> add highlight -> pause -> reset
+       *
+       * @param {string} phrase
+       * @param {number} [hold]
+       */
       handle.typePhrase = function typePhrase(phrase, hold) {
         this
           .type(phrase)
-          .pause(500).call(addHighlight)
+          .pause(500).addClass(HIGHLIGHT_CLASS)
           .pause(hold).reset();
         return this;
       };
 
+      /**
+       * Types "The man. The Myth. The legend" Sequence
+       */
       handle.typeLegendTaunt = function typeLegendTaunt() {
         this
           .type('The Man')
@@ -97,9 +184,9 @@ const malarkey = require('malarkey');
           .pause(750).type('Myth', 100)
           .pause(1000).delete(4, 0)
           .pause(750).type('Legend', 500)
-          .pause(500).call(addHighlightLegend)
+          .pause(500).addClass(HIGHLIGHT_CLASS).addClass(HIGHLIGHT_CLASS_LEGEND)
           .pause(2000).delete()
-          .pause(300).reset().call(removeHighlightLegend)
+          .pause(300).reset().removeClass(HIGHLIGHT_CLASS_LEGEND)
           .pause(500);
         return this;
       };
